3bc3044b5ecef6637c208d47c692d3cfb95e95ee
mellikeozkan <115733605+mellikeozkan@users.noreply.github.com>
2022-12-01 11:23:29 +0300

    Add files via upload

---
+++ MAGarackiralama/frmanasayfa.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace MAGarackiralama
+{
+    public partial class frmanasayfa : Form
+    {
+        public frmanasayfa()
+        {
+            initializeComponent();
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
